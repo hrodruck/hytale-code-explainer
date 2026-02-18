0606be1c11d39e7711a77ed75d8f0343d2836564
Rodrigo Mello <hrodruck@gmail.com>
2026-02-18 11:00:49 -0300

    Update code to Hytale Patch 3

--- scripts/qdrant_import.py
+++ scripts/qdrant_import.py
@@ -3,5 +3,5 @@ from qdrant_client import QdrantClient
 client = QdrantClient("localhost", port=6333)
 client.recover_snapshot(
     collection_name="hytale_codebase",
-    location="file:///snapshots/hytale_codebase-6604404928335678-2026-02-02-13-26-51.snapshot"
+    location="file:///snapshots/hytale_codebase-1863882730595526-2026-02-18-13-57-13.snapshot"
 )
